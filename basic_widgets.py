5fde390ed9704e9a0752fd6f0cfd04f25a73e1b0
Jo <johanna.a.k.tjernstrom@gmail.com>
2022-06-21 17:02:26 +0200

    updating widgets based on site, model works. Implemented in time series

--- basic_widgets.py
+++ basic_widgets.py
@@ -3,37 +3,79 @@ import panel as pn
 from thredds_crawler.crawl import Crawl
 import datetime as dt
 import functools as fn
+import json
+
+def populate_vars_1D():
+    file = open("site_model.json")
+    site_mod_dict = json.load(file)
+    
+    base = "https://thredds.met.no/thredds/catalog/alertness/YOPP_supersite/"
+    for site in site_mod_dict.keys():
+        for model in site_mod_dict[site].keys():
+            url = base+model+'/'+site+'/catalog.html'
+            regex = '.*'+"0201"+'00\.nc'
+            file = Crawl(url, select=[regex]).datasets[0].services[0]['url']
+            ds = xr.open_dataset(file)
+
+            variables = []
+            for name, stuff in ds.data_vars.items():
+                if len(stuff.coords.dims) == 1 and stuff.coords.dims[0] == 'time':
+                    #needs additional filter for wind stuff and other vars that need calculating
+                    variables.append(name)
+
+            site_mod_dict[site][model] = variables
+            
+    return site_mod_dict
+
 
 #Create the widgets and set dependencies, returns a list of widgets ========================
-def create_widgets():
+def create_widgets(site_mod_dict):
+    
+    _sites_models = {i:list(site_mod_dict[i].keys()) for i in list(site_mod_dict.keys())}
+    #print(_sites_models)
+    
     #widgets that are dependent on each other =================================================
     #==========================================================================================
-    site_name = pn.widgets.Select(
-        name="Site", options=['sodankyla','tiksi'], margin=(0, 20, 0, 0)
+    sites = pn.widgets.Select(
+    value=list(_sites_models.keys())[0], 
+    options=list(_sites_models.keys())
     )
 
-
-    model_name = pn.widgets.Select(
-        name="model", options=[], margin=(0, 20, 0, 0)
+    model = pn.widgets.Select(
+    value=_sites_models[sites.value][0], 
+    options=_sites_models[sites.value]
     )
-
 
     variable = pn.widgets.Select(
-        name="Variables", options=[], margin=(0, 20, 0, 0)
+        value=site_mod_dict[sites.value][model.value][0],
+    options=site_mod_dict[sites.value][model.value]
     )
+
+    @pn.depends(sites.param.value, watch=True)
+    def _update_countries(sites):
+        models = _sites_models[sites]
+        model.options = models
+        model.value = models[0]
+
+    #@pn.depends(sites.param.value, watch=True)
+    @pn.depends(sites.param.value,model.param.value, watch=True)
+    def _update_var(sites,model):
+        variables = site_mod_dict[sites][model]
+        variable.options = variables
+        variable.value = variables[0]
 
     #==========================================================================================
     #select SOP based on avaiable files? assume it exists for all?
     #disable SOP choices based on file availability? how easily check this?
-    sop = pn.widgets.Select(
-        name="SOP", options=['SOP1','SOP2'], margin=(0, 20, 0, 0)
-    )
+    #sop = pn.widgets.Select(
+    #    name="SOP", options=['SOP1','SOP2'], margin=(0, 20, 0, 0)
+    #)
 
     #set dates based on selected SOP
     dates = pn.widgets.DateRangeSlider(
         name='Date Range Slider',
         start=dt.datetime(2018, 2, 2), end=dt.datetime(2018, 3, 31),
-        value=(dt.datetime(2018, 2, 2), dt.datetime(2018, 3, 31))
+        value=(dt.datetime(2018, 2, 2), dt.datetime(2018, 2, 10))
     )
 
     #widgets that remain constant regardless of model selected=================================
@@ -45,63 +87,10 @@ def create_widgets():
     start_time = pn.widgets.Select(
         name="Start time", options=['00','12'], margin=(0, 20, 0, 0)
     )
-
-    site_name.link(model_name,callbacks={'value':update_models})
-    site_name.value = ' '
     
-   # model_name.param.watch(fn.partial(update_variables,site_name = site_name,dates = dates),['value'])
-    #model_name.param.trigger('value')
-    model_name.link(variable,callbacks={'value':fn.partial(update_variables,site = site_name.value,dates = dates)})
-    #model_name.value = ' '
-
-    #sop.link(dates,callbacks={'value':update_dates})
-    #sop.value = ' '
-    #sop.param.watch(update_dates,['value'])
-    #sop.param.trigger('value')
 
     
-    widgets = [site_name,model_name,variable,dates,sop,concat_day,start_time]
+    widgets = [sites,model,variable,dates,concat_day,start_time]
 
     return widgets
 
-#Update functions for updates triggered by changes in dependant widgets =======================
-def update_models(target,event):
-    site  = event.new
-    if site == 'sodankyla':
-        models = ['AROME-Arctic','ifs-ecmwf'] #TO DO: do list models that are available for sodankyla read in from portal
-        target.options = models
-    elif site == 'tiksi':
-        models = ['ifs-ecmwf']  #TO DO: do list models that are available for sodankyla read in from portal
-        target.options = models
-        
-        
-def update_variables(events,site,dates):
-    model = events.new
-    st = dates.value[0].strftime("%Y-%m-%d").replace("-","")
-    base = 'https://thredds.met.no/thredds/catalog/alertness/YOPP_supersite/'
-    
-    url = base+model+'/'+site+'/catalog.html'
-    regex = '.*'+st+'00\.nc'
-    file = Crawl(url, select=[regex]).datasets[0].services[0]['url']
-
-    ds = xr.open_dataset(file)
-
-    ds_variables = ds.data_vars
-    variables = {}
-    for name, stuff in ds_variables.items():
-        if len(stuff.coords.dims) == 1 and stuff.coords.dims[0] == 'time':
-            #needs additional filter for wind stuff and other vars that need calculating
-            variables[stuff.attrs['long_name']] = name
-
-    variable.options = variables
-
-def update_dates(event,target):
-    sop_choice = event.new
-    if sop_choice == 'SOP1':
-        target.start = dt.datetime(2018, 2, 1)
-        target.end = dt.datetime(2018, 3, 31)
-        target.value = (dt.datetime(2018, 2, 2),dt.datetime(2018, 3, 31))
-    elif sop_choice == 'SOP2':
-        target.start = dt.datetime(2018, 7, 1)
-        target.end = dt.datetime(2018, 9, 30)
-        target.value = (dt.datetime(2018, 7, 1),dt.datetime(2018, 9, 30))
